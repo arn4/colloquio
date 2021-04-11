484dea4c9a3ea04e675ba8331cad57ef57f079f7
Luca Arnaboldi <luca@arnaboldi.lu>
2021-04-11 17:56:50 +0200

    Begin of simulations and reordering

---
+++ simulations/mnist-generator.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <thread>
+#include <cstdlib>
+
+#include <BinaryRBM.hpp>
+#include <TrainingSet.hpp>
+#include <PersistentContrastiveDivergence.hpp>
+#include <MarcovChain.hpp>
+
+
+using namespace std;
+using namespace rbm;
+
+//const size_t   DIGITS = 10; // from 0 to 9
+const size_t   PIXELS = 28*28;
+const unsigned HIDDEN_SIZE =500;
+const unsigned DEFAULT_SEED = 64770;
+const unsigned SAMPLES = 20;
+const unsigned STEPS_TO_STATIONARY = 400;
+using real_value = double;
+
+int main(int argc, char *argv[]) {
+  unsigned seed;
+  if (argc > 1) {
+    seed = strtoul(argv[1], nullptr, 10);
+  } else {
+    seed = DEFAULT_SEED;
+  }
+  clog << "Using seed = " << seed << endl << endl;
+  mt19937 rng(seed);
+
+  // Generate a random RBM
+  clog << "Loading the RBM... ";
+  BinaryRBM<real_value> rbm_CD1(PIXELS, HIDDEN_SIZE, rng);
+  BinaryRBM<real_value> rbm_PCD1(PIXELS, HIDDEN_SIZE, rng);
+  
+  
+  rbm_CD1.load_from_file("mnist-cd1.rbm");
+  rbm_PCD1.load_from_file("mnist-pcd1.rbm");
+  clog << "Done!" << endl << endl;
+
+  
+  // Generate TestSet
+  clog << "Generation of the samples... " << endl;
+  vector<vector<bool>> samples_CD1;
+  vector<vector<bool>> samples_PCD1;
+  MarcovChain<real_value> mc_CD1(rbm_PCD1, rng);
+  MarcovChain<real_value> mc_PCD1(rbm_PCD1, rng);
+
+  for (unsigned i = 1; i <= SAMPLES; i++) {
+    mc_CD1.init_random_v();
+    mc_CD1.evolve(STEPS_TO_STATIONARY);
+    auto sample = mc_CD1.v();
+    samples_CD1.push_back(vector<bool>(sample.begin(), sample.end()));
+
+    mc_PCD1.init_random_v();
+    mc_PCD1.evolve(STEPS_TO_STATIONARY);
+    sample = mc_PCD1.v();
+    samples_PCD1.push_back(vector<bool>(sample.begin(), sample.end()));
+  }
+  clog << "Done!" << endl << endl;
+    
+  clog << "Writing on file... " << endl;
+  ofstream cd1_out("./samples-CD1.txt");
+  ofstream pcd1_out("./samples-PCD1.txt");
+
+  for (auto s: samples_CD1) {
+    for (bool b: s) {
+      cd1_out << b << ' ';
+    }
+    cd1_out << endl;
+  }
+  cd1_out.close();
+
+  for (auto s: samples_PCD1) {
+    for (bool b: s) {
+      pcd1_out << b << ' ';
+    }
+    pcd1_out << endl;
+  }
+  pcd1_out.close();
+   
+  return 0;
+}
---
+++ simulations/mnist.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <thread>
+#include <cstdlib>
+#include <future>
+
+#include <BinaryRBM.hpp>
+#include <TrainingSet.hpp>
+#include <PersistentContrastiveDivergence.hpp>
+#include <MarcovChain.hpp>
+
+
+using namespace std;
+using namespace rbm;
+
+const size_t   DIGITS = 10; // from 0 to 9
+const size_t   PIXELS = 28*28;
+const size_t   TRAINING_SET_SIZE = 60000;
+const unsigned HIDDEN_SIZE = 500;
+const unsigned DEFAULT_SEED = 64770;
+const unsigned EPOCHS = 50;
+using real_value = double;
+const real_value LEARNING_RATE = 0.0001;
+const real_value WEIGHT_DECAY = 0.0001;
+const real_value MOMENTUM = 0.;
+
+using CD = ContrastiveDivergence<real_value, PIXELS, DIGITS>;
+using PCD = PersistentContrastiveDivergence<real_value, PIXELS, DIGITS>;
+
+int main(int argc, char *argv[]) {
+  unsigned seed;
+  if (argc > 1) {
+    seed = strtoul(argv[1], nullptr, 10);
+  } else {
+    seed = DEFAULT_SEED;
+  }
+  clog << "Using seed = " << seed << endl << endl;
+  mt19937 rng(seed);
+
+  // Reading files
+  clog << "Reading from file... " << endl;
+  vector<vector<vector<bool>>> classes(DIGITS);
+  ifstream mnist("./mnist-train.txt");
+  for (unsigned i = 1; i <= TRAINING_SET_SIZE; i++) {
+    unsigned label;
+    mnist >> label;
+    vector<bool> tmp(PIXELS);
+    for (unsigned q = 0; q < PIXELS; q++) {
+      unsigned c;
+      mnist >> c;
+      tmp[q] = (c==1);
+    }
+    classes[label].push_back(tmp);
+    //classes[i%DIGITS].push_back(tmp);
+  }
+  mnist.close();
+
+  clog << "Class sizes: ";
+  for (unsigned c = 0; c < DIGITS; c++) {
+    clog << classes[c].size() << ' ';
+  }
+  clog  << endl << endl;
+
+  //Build training set
+  clog << "Building Training Set..." << endl;
+  TrainingSet<PIXELS, DIGITS> ts(classes, rng);
+  clog << "Number of batches: " << ts.num_of_batches() << endl << endl;
+
+  // Training 
+  clog << "Training... " << endl;
+
+  BinaryRBM<real_value> rbm_cd1 (PIXELS, HIDDEN_SIZE, rng);
+  BinaryRBM<real_value> rbm_pcd1(PIXELS, HIDDEN_SIZE, rng);
+
+  CD cd1(rbm_cd1, ts, 1, rng, LEARNING_RATE, WEIGHT_DECAY, MOMENTUM);
+  PCD pcd1(rbm_pcd1, ts, 1, rng, LEARNING_RATE, WEIGHT_DECAY, MOMENTUM);
+
+  ofstream result("mnist-psl-"+to_string(seed)+".txt");
+  for (unsigned e = 1; e <= EPOCHS; e++) {
+    clog << "Epoch " << e << endl;
+    thread epoch_cd1(&CD::epoch, cd1, 0);
+    thread epoch_pcd1(&PCD::epoch, pcd1, 0);
+    epoch_cd1.join();
+    epoch_pcd1.join();
+
+    auto future_psl_cd1 = async(&CD::log_pseudolikelihood, cd1);
+    auto future_psl_pcd1 = async(&PCD::log_pseudolikelihood, pcd1);
+
+    real_value psl_cd1  = future_psl_cd1.get();
+    real_value psl_pcd1 = future_psl_pcd1.get();
+
+    clog << "  PSL CD-1: "  << psl_pcd1 << endl;
+    clog << "  PSL PCD-1: " << psl_cd1  << endl;
+
+    result << e;
+    result << ' ' << psl_cd1;
+    result << ' ' << psl_pcd1;
+    result << endl;
+  }
+  result.close();
+  clog << "Done!" << endl << endl;
+  rbm_cd1.save_on_file("mnist-cd1-"+to_string(seed)+".rbm");
+  rbm_pcd1.save_on_file("mnist-pcd1-"+to_string(seed)+".rbm");
+}
